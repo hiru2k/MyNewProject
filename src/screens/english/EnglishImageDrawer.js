e1a2a6b1299ee05d1faf4bba38ee2a2a6c1e282f
hiru2k <mudannayakehiruni90@gmail.com>
2023-11-06 07:42:10 +0530

    added cat img

--- src/screens/english/EnglishImageDrawer.js
+++ src/screens/english/EnglishImageDrawer.js
@@ -11,6 +11,8 @@ import {
 } from "react-native";
 import emptyTreeImage from "../../assets/empty_tree.png";
 import coloredTreeImage from "../../assets/colored_tree.png";
+import emptyCatImage from "../../assets/empty_cat.png";
+import coloredCatImage from "../../assets/colored_cat.png";
 import ProgressBar from "../../components/ProgressBar";
 import ColorPalette from "../../components/ColorPalette";
 import { useNavigation } from "@react-navigation/native";
@@ -39,22 +41,31 @@ const EnglishImageDrawer = ({ route }) => {
     "grey",
     "grey",
   ];
+  const catColors = [
+    "#fff200",
+    "#c3c3c3",
+    "#000000",
+    "#ffaec8",
+    "#88001b",
+    "grey",
+    "grey",
+  ];
   const greyColors = ["grey", "grey", "grey", "grey", "grey", "grey", "grey"];
 
   let selectedImage;
   let colorsToDisplay;
   switch (word) {
-    case "tree":
-      selectedImage = !isColored ? coloredTreeImage : emptyTreeImage;
-      colorsToDisplay = !isColored ? greyColors : treeColors;
+    case "Tree":
+      selectedImage = isColored ? coloredTreeImage : emptyTreeImage;
+      colorsToDisplay = isColored ? greyColors : treeColors;
       break;
-    case "flower":
-      selectedImage = !isColored ? flowerImage : emptyTreeImage;
-      colorsToDisplay = !isColored ? greyColors : treeColors;
+    case "Cat":
+      selectedImage = isColored ? coloredCatImage : emptyCatImage;
+      colorsToDisplay = isColored ? greyColors : catColors;
       break;
     default:
-      selectedImage = !isColored ? coloredTreeImage : emptyTreeImage;
-      colorsToDisplay = !isColored ? greyColors : treeColors;
+      selectedImage = isColored ? coloredTreeImage : emptyTreeImage;
+      colorsToDisplay = isColored ? greyColors : treeColors;
   }
 
   const checkProgress = () => {
